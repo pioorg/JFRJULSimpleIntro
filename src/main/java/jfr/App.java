8b2bf856d716e179c2ace496e457b6f0b12075f6
Piotr <piotr.przybyl@vectus.pl>
2019-01-19 00:39:52 +0100

    Initial commit.

---
+++ src/main/java/jfr/App.java
@@ -0,0 +1,51 @@
+/**
+ * This is based on https://dzone.com/articles/using-java-flight-recorder-with-openjdk-11-2
+ */
+package jfr;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Stream;
+
+public class App {
+    static BlockingQueue<byte[]> queue = new LinkedBlockingQueue<>();
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("I've got the following arguments:");
+            Stream.of(args).forEach(System.out::println);
+        }
+        new Thread(new Consumer()).start();
+        new Thread(new Producer()).start();
+
+    }
+
+    static class Producer implements Runnable {
+        public void run() {
+            while (true) {
+                queue.offer(new byte[3 * 1024 * 1024]);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class Consumer implements Runnable {
+
+        public void run() {
+            while (true) {
+                try {
+                    queue.take();
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+}
